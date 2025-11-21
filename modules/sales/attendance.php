be27af689574e0fa8c67293365e67d3a750bebb2
dev-osama74 <y648twm8sg@privaterelay.appleid.com>
2025-11-21 19:41:33 +0200

    Update - 2025-11-21 19:41

--- modules/sales/attendance.php
+++ modules/sales/attendance.php
@@ -1,3 +1,17 @@
 <?php
-header('Location: ../../attendance.php');
+// استخدام JavaScript redirect بدلاً من header redirect
+// لأن header.php تم تضمينه بالفعل وأرسل output
+
+// التأكد من أن path_helper.php تم تضمينه
+if (!function_exists('getBasePath')) {
+    require_once __DIR__ . '/../../includes/path_helper.php';
+}
+
+$basePath = function_exists('getBasePath') ? getBasePath() : '';
+$attendanceUrl = rtrim($basePath, '/') . '/attendance.php';
+?>
+<script>
+window.location.href = '<?php echo htmlspecialchars($attendanceUrl, ENT_QUOTES, 'UTF-8'); ?>';
+</script>
+<?php
 exit;   
