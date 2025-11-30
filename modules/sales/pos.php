24b34d14f214838c2a0dafe9baf4a47d038db4b7
dev-osama74 <y648twm8sg@privaterelay.appleid.com>
2025-11-30 18:25:14 +0200

    Update - 2025-11-30 18:25

--- modules/sales/pos.php
+++ modules/sales/pos.php
@@ -2146,7 +2146,7 @@ if (!$error) {
                                 </div>
                                 <div class="mt-3 d-none" id="posPartialWrapper">
                                     <label class="form-label">مبلغ التحصيل الجزئي</label>
-                                    <input type="number" class="form-control" id="posPartialAmount" placeholder="0">
+                                    <input type="number" class="form-control" id="posPartialAmount" placeholder="0" required>
                                 </div>
                                 <div class="mt-3 d-none" id="posDueDateWrapper">
                                     <label class="form-label">تاريخ الاستحقاق <span class="text-muted">(اختياري)</span></label>
