537e2507b83317f7ab152547943bcdd1e54e9951
dev-osama74 <y648twm8sg@privaterelay.appleid.com>
2025-12-04 03:32:14 +0200

    Update - 2025-12-04 03:32

--- modules/accountant/invoices.php
+++ modules/accountant/invoices.php
@@ -153,9 +153,6 @@ if (isset($_GET['id'])) {
 ?>
 <div class="d-flex justify-content-between align-items-center mb-4">
     <h2><i class="bi bi-receipt me-2"></i>إدارة الفواتير</h2>
-    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInvoiceModal">
-        <i class="bi bi-plus-circle me-2"></i>إنشاء فاتورة
-    </button>
 </div>
 
 <?php if ($error): ?>
